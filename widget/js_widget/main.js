a8e104ca9ab6f6f713888e84dd4b9b00b9748f8c
Andrei <90472447+AndreisLavrov@users.noreply.github.com>
2022-12-20 09:31:02 +0300

    Update main.js

--- widget/js_widget/main.js
+++ widget/js_widget/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var flag = 0;  
     $("button#changeQR").click(function(){
       if(flag == 0) {
-        $("#widget").attr("src","https://test.ecom.raiffeisen.ru/api/sbp/v1/qr/ADC2BB6468604F1E854690448314DA3A/imagehttps://test.ecom.raiffeisen.ru/api/sbp/v1/qr/ASCC39EF9FD64A34B391722CA15A12DA/image");
+        $("#widget").attr("src","https://test.ecom.raiffeisen.ru/api/sbp/v1/qr/ADC2BB6468604F1E854690448314DA3A/image");
         flag = 1;
       }
       else if(flag == 1) {
