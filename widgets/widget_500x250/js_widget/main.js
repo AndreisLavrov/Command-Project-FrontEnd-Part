5bfad954a14f3585dc00f2398f821a1957b2c75c
Andrei <90472447+AndreisLavrov@users.noreply.github.com>
2023-01-08 14:35:49 +0300

    widget 08.01

--- widgets/widget_500x250/js_widget/main.js
+++ widgets/widget_500x250/js_widget/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     var flag = 0;  
-    $("button#changeQR").click(function(){
+    $("input#slideThree").click(function(){
       if(flag == 0) {
-        $("#widget").attr("src","https://test.ecom.raiffeisen.ru/api/sbp/v1/qr/ADC2BB6468604F1E854690448314DA3A/imagehttps://test.ecom.raiffeisen.ru/api/sbp/v1/qr/ASCC39EF9FD64A34B391722CA15A12DA/image");
+        $("#widget").attr("src","https://test.ecom.raiffeisen.ru/api/sbp/v1/qr/ADC2BB6468604F1E854690448314DA3A/image");
         flag = 1;
       }
       else if(flag == 1) {
